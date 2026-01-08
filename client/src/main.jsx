2e50623dceaf187acfd1c855dd506d88d6548c04
unknown <seharwatunnati@gmail.com>
2026-01-08 20:05:28 +0530

    Fix import casing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import AppContextProvider from './context/Appcontext.jsx'
+import AppContextProvider from './context/AppContext.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-      <AppContextProvider>
-          <App />
-      </AppContextProvider>
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
   </BrowserRouter>
 )
