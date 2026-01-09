c6343e69379c3ec03276341e08dd161669191914
unknown <seharwatunnati@gmail.com>
2026-01-09 18:41:44 +0530

    fixing minor bugs

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -90,10 +90,21 @@ export const userCredits = async (req, res) => {
 }
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
+    }
+});
+
+// Verify connection configuration
+transporter.verify(function (error, success) {
+    if (error) {
+        console.log("SMTP Connection Error:", error);
+    } else {
+        console.log("SMTP Server is ready to take our messages");
     }
 });
 
